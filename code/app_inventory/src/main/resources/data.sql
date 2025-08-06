8d0ca0005b49edf20dddea6e0012edbe47de0656
Milos Milic <mmilic@gmail.com>
2025-08-06 14:48:30 +0200

    Added app_inventory

---
+++ code/app_inventory/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO inventory(name, description, measurement_unit, quantity) VALUES ('Inventory 1', 'Gerlach-Rath desc1', 'PCS', 999999);
+INSERT INTO inventory(name, description, measurement_unit, quantity) VALUES ('Inventory 2', 'Swift, Turcotte and Haley desc2', 'PCS', 999999);
+INSERT INTO inventory(name, description, measurement_unit, quantity) VALUES ('Inventory 3', 'Schneider Group desc3', 'PCS', 999999);
+INSERT INTO inventory(name, description, measurement_unit, quantity) VALUES ('Inventory 4', 'Keeling, Schultz and Kiehn desc4', 'PCS', 999999);
+INSERT INTO inventory(name, description, measurement_unit, quantity) VALUES ('Inventory 5', 'Hackett LLC desc5', 'PCS', 999999);
